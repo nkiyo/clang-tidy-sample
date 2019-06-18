baa3ef02760ee6a9528dca9182c9269b95aa4cb5
nkiyo <nkiyo@hoge.com>
2019-06-18 09:51:56 +0900

    init commit

---
+++ file.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char ** /*argv*/)
+{
+  //const char *cpc;
+  //const char *cpc2 = (const char*)cpc;
+  int b = argc;
+  //short b = argc;
+  printf("hoge = %d\n", b);
+  return b;
+}
